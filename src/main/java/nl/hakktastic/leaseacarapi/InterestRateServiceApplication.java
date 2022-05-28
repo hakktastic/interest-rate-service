230b7fd47e08f259ad685b7e7d4ae76f8bbd6d95
hakktastic <hakktastic@gmail.com>
2022-05-28 13:27:24 +0200

    Initial commit

---
+++ src/main/java/nl/hakktastic/leaseacarapi/InterestRateServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.hakktastic.leaseacarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestRateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterestRateServiceApplication.class, args);
+    }
+
+}
