8a8c83545d33c36ae4ddd162a2c37715c9a151d5
hakktastic <hakktastic@gmail.com>
2022-07-18 18:12:39 +0200

    HAK-40 add package-info.java
    for package-level documentation and annotations

---
+++ src/main/java/nl/hakktastic/leaseacarapi/repository/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package is about the {@link org.springframework.data.jpa.repository.JpaRepository} objects of the REST service.
+ *
+ */
+@NonNullApi
+@NonNullFields
+package nl.hakktastic.leaseacarapi.repository;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
